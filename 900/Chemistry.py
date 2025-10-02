4fa1c9d064c5a25fc0fbe8f80c277e8b1dccb8a6
Pathi Krishna Kanth <krishnakanth@Pathis-MacBook-Air.local>
2025-10-02 09:47:39 +0530

    added new

--- 900/Chemistry.py
+++ 900/Chemistry.py
@@ -1,29 +1,28 @@
-    from collections import Counter
-    t = int(input())
+from collections import Counter
+t = int(input())
 
 
-    for _ in range(t):
-        n , k = map(int, input().split())
-        a = input()
-        hashmap = Counter(a)
-        # print(hashmap)
-        larget_odd = even = 0
-        total = 0
-        for key in hashmap:
-            if hashmap[key] % 2 == 1:
-                larget_odd = max(larget_odd, hashmap[key])
-                total += hashmap[key]
-            else:
-                even += hashmap[key]
-        total -= larget_odd
-        if k == total :
-            print("YES")
-        elif k < total:
-            print("NO")
+for _ in range(t):
+    n , k = map(int, input().split())
+    a = input()
+    hashmap = Counter(a)
+    # print(hashmap)
+    larget_odd = even = 0
+    total = 0
+    for key in hashmap:
+        if hashmap[key] % 2 == 1:
+            larget_odd = max(larget_odd, hashmap[key])
+            total += hashmap[key]
         else:
-            k2 = k - total
-            if k2 <= even + larget_odd :
-                print("YES")
-            else:
-                print("NO")
-        # print(total , k)z
+            even += hashmap[key]
+    total -= larget_odd
+    if k == total :
+        print("YES")
+    elif k < total:
+        print("NO")
+    else:
+        k2 = k - total
+        if k2 <= even + larget_odd :
+            print("YES")
+        else:
+            print("NO")
